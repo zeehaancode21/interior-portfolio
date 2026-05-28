bbe06b08c1f86b11e87f2a1cb2e5928ea8f3d02a
zeehaancode21 <zeehaancode@gmail.com>
2026-05-28 19:55:02 +0530

    update portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/interior-portfolio/">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
